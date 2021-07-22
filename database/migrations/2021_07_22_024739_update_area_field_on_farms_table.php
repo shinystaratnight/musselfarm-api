bf62ab3d97ca1c75aff2a4fa222e4c69e72605e1
FlynnRider-000 <coronarider01@gmail.com>
2021-07-22 05:48:58 +0300

    Update Farm area float

---
+++ database/migrations/2021_07_22_024739_update_area_field_on_farms_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateAreaFieldOnFarmsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('farms', function (Blueprint $table) {
+            $table->float('area')->default(0.00)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
